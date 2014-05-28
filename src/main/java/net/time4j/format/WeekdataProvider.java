68400267bb44bcab91d03f99f73a4db7ea8a5b83
Meno Hochschild <mhochschild@gmx.de>
2014-05-29 01:22:04 +0200

    license change for every java file header
    see issue #12

--- src/main/java/net/time4j/format/WeekdataProvider.java
+++ src/main/java/net/time4j/format/WeekdataProvider.java
@@ -1,20 +1,20 @@
 /*
  * -----------------------------------------------------------------------
- * Copyright © 2012 Meno Hochschild, <http://www.menodata.de/>
+ * Copyright © 2013-2014 Meno Hochschild, <http://www.menodata.de/>
  * -----------------------------------------------------------------------
  * This file (WeekdataProvider.java) is part of project Time4J.
  *
- * Time4J is free software: You can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * Time4J is free software: You can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * Time4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with Time4J. If not, see <http://www.gnu.org/licenses/>.
  * -----------------------------------------------------------------------
  */
