73e324c904e6408fcf42630ebb532407119ec869
Meno Hochschild <mhochschild@gmx.de>
2014-06-23 17:16:58 +0200

    english translation of TimePoint
    see issue #4

--- src/main/java/net/time4j/engine/TimePoint.java
+++ src/main/java/net/time4j/engine/TimePoint.java
@@ -27,6 +27,88 @@ import java.io.Serializable;
 
 
 /**
+ * <p>Represents an immutable time point along a time axis which is directed
+ * into the future. </p>
+ *
+ * <h4>Display and change chronological element values</h4>
+ *
+ * <p>The time point consists of chronological elements. This base class
+ * delegates the time arithmetic to the associated time axis respective to
+ * the underlying rules of elements and units. However, any concrete subclass
+ * is required to define the state and reflect it in all {@code get()}-methods
+ * and also to specify the serialization behaviour. </p>
+ *
+ * <p>Element values can only be changed by creating a new immutable copy
+ * of the original instance. This is done via all {@code with()}-methods. </p>
+ *
+ * <h4>Time axis</h4>
+ *
+ * <p>If the referenced time axis is the UTC-timeline (that is a time point
+ * is defined relative to the start of UTC epoch - see 
+ * <a href="package-summary.html">package summary</a>) then any implementation
+ * must also implement the interface {@link net.time4j.scale.UniversalTime}.
+ * In every other case we have a local time axis. All time units are to be
+ * defined referencing the time axis. For example, second units are interpreted
+ * as local UT1-seconds on a local timestamp but on a {@code UniversalTime} 
+ * before 1972 as global UT1-seconds and after 1972 as atomic SI-seconds.
+ * Hence Time4J has even defined different second units in the main package.
+ * Applications should therefore take much care if they transform a duration
+ * from one time axis to another one. </p>
+ *
+ * <h4>Sorting</h4>
+ *
+ * <p>Unless explicitly stated otherwise sorting of time points is always
+ * in strict temporal order and consistent with {@code equals()}. In case
+ * of doubt the documentation of the subclass is leading. Alternatively,
+ * subclasses are free to implement the interface {@code Temporal} to
+ * enable a temporal order. </p>
+ *
+ * <h4>Addition (or subtraction) of a time span to a time point</h4>
+ *
+ * <p>These operations are performed by all {@code plus()}- and {@code minus()}-
+ * methods. A time span can either be a single time unit, or it consists of
+ * several time units. </p>
+ *
+ * <p>If given time unit does not have a fixed length (for example months)
+ * then the result of an addition can deviate from the expected element
+ * value to be considered. In case of multiple additions care is required.
+ * In case of doubt the original value should be saved for a later addition.
+ * Example with additions of months in one or two steps (pseudo-code): </p>
+ *
+ * <ul>
+ *  <li>[2011-08-31] + [P1M] = [2011-09-30]</li>
+ *  <li>[2011-09-30] + [P1M] = [2011-10-30]</li>
+ *  <li>[2011-08-31] + [P2M] = [2011-10-31]</li>
+ * </ul>
+ *
+ * <h4>Difference of time points</h4>
+ *
+ * <p>The difference of time points results in a time span. The result can
+ * either be expressed in one time unit only, or in multiple units which
+ * represent the base unit of associated chronological element. In latter
+ * case users have to define a metric, too. </p>
+ *
+ * <h4>Implementation notes</h4>
+ *
+ * <ul>
+ *  <li>All subclasses must be <i>final</i> und <i>immutable</i>. </li>
+ *  <li>Documentation of supported and registered elements is required. </li>
+ *  <li>For a suitable choice of the type U of time units it should be
+ *  noted that the time units must correspond to the internal state of
+ *  a time point because otherwise the calculation of a time span between
+ *  two time points cannot be complete. For example it would be a mistake
+ *  to allow milliseconds for a time point which even has nanoseconds. </li>
+ *  <li>The natural order should be consistent with {@code equals()}. </li>
+ * </ul>
+ *
+ * @param   <U> generic type of time units compatible to {@link ChronoUnit}
+ * @param   <T> generic type of self reference
+ * @author  Meno Hochschild
+ * @see     Chronology
+ * @see     TimeAxis
+ * @see     Temporal
+ */
+/*[deutsch]
  * <p>Repr&auml;sentiert einen unver&auml;nderlichen Zeitpunkt auf einer in
  * die Zukunft gerichteten Zeitachse. </p>
  *
@@ -35,8 +117,9 @@ import java.io.Serializable;
  * <p>Der Zeitwert setzt sich aus chronologischen Elementen zusammen. Diese
  * abstrakte Basisklasse delegiert die Zeitrechnung immer an die zugeh&ouml;rige
  * Zeitachse bzw. genauer an die ihr zugeordneten Regeln der Elemente und
- * Zeiteinheiten, mu&szlig; aber in den {@code ChronoEntity}-Methoden selbst
- * den Zustand definieren und auch das Serialisierungsverhalten festlegen. </p>
+ * Zeiteinheiten, mu&szlig; aber selbst den Zustand definieren, in den
+ * {@code get()}-Methoden den Zustand reflektieren und auch das
+ * Serialisierungsverhalten festlegen. </p>
  *
  * <p>Da alle konkreten Implementierungen <i>immutable</i> sind und sein
  * m&uuml;ssen, sind Elementwerte nur dadurch &auml;nderbar, da&szlig; jeweils
@@ -55,9 +138,7 @@ import java.io.Serializable;
  * mit externem Zeitzonenkontext, auf einem {@code UniversalTime}-Objekt
  * vor 1972 als globale UT1-Sekunden und nach 1972 als atomare SI-Sekunden.
  * Deshalb sollten Anwendungen besondere Vorsicht walten lassen, wenn eine
- * Dauer von einer Zeitachse auf eine andere Zeitachse &uuml;bertragen wird.
- * Eine geeignete Konversionsmethode kann z.B. die Klasse {@code TimeScale}
- * bieten. </p>
+ * Dauer von einer Zeitachse auf eine andere Zeitachse &uuml;bertragen wird. </p>
  *
  * <h4>Sortierung</h4>
  *
@@ -77,11 +158,11 @@ import java.io.Serializable;
  * <p>Wenn die angegebene Zeiteinheit bzw. Genauigkeit keine konstante
  * L&auml;nge definiert (zum Beispiel haben Monate in Tagen ausgedr&uuml;ckt
  * im allgemeinen unterschiedliche L&auml;ngen), dann kann das Ergebnis
- * einer Addition im Hinblick auf den erwarteten Elementwert {@code amount}
- * abweichen, siehe Regel 3. Bei mehrfachen Additionen hintereinander ist
- * Vorsicht angebracht, im Zweifelsfall sollte der Originalwert f&uuml;r
- * eine sp&auml;tere Addition gespeichert werden. Beispiel mit Additionen
- * von Monaten in einem oder in zwei Schritten (Pseudo-Code): </p>
+ * einer Addition im Hinblick auf den erwarteten Elementwert abweichen. Bei
+ * mehrfachen Additionen hintereinander ist Vorsicht angebracht, im
+ * Zweifelsfall sollte der Originalwert f&uuml;r eine sp&auml;tere Addition
+ * gespeichert werden. Beispiel mit Additionen von Monaten in einem oder in
+ * zwei Schritten (Pseudo-Code): </p>
  *
  * <ul>
  *  <li>[2011-08-31] + [P1M] = [2011-09-30]</li>
@@ -128,6 +209,17 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     //~ Methoden ----------------------------------------------------------
 
     /**
+     * <p>Compares two time points preferably by their temporal positions
+     * on the common time axis. </p>
+     *
+     * <p>Implementation note: In order to make the natural order consistent
+     * with {@code equals()} the whole state must be taken into account,
+     * with preference for those attributes which define the temporal
+     * position on the time axis. </p>
+     *
+     * @see     #equals(Object)
+     */
+    /*[deutsch]
      * <p>Vergleicht zwei Zeitpunkte bevorzugt nach ihrer Position auf der
      * gemeinsamen Zeitachse. </p>
      *
@@ -142,14 +234,28 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     public abstract int compareTo(T timePoint);
 
     /**
+     * <p>Adds the given time span to this time point and yields
+     * the result of the addition. </p>
+     *
+     * <p>Delegates to {@link TimeSpan#addTo(TimePoint)}. </p>
+     *
+     * @param   timeSpan    time span to be added to this instance
+     * @return  result of addition as changed copy, this
+     *          instance remains unaffected
+     * @throws  RuleNotFoundException if any time unit is not registered
+     *          and does also not implement {@link UnitRule.Source}
+     * @throws  ArithmeticException in case of numerical overflow
+     * @see     #minus(TimeSpan)
+     */
+    /*[deutsch]
      * <p>Addiert die angegebene Zeitspanne zur Bezugszeit und liefert das
      * Additionsergebnis zur&uuml;ck. </p>
      *
      * <p>Delegiert an {@link TimeSpan#addTo(TimePoint)}. </p>
      *
      * @param   timeSpan    time span to be added to this instance
-     * @return  result of addition as changed copy, this instance
-     *          remains unaffected
+     * @return  result of addition as changed copy, this
+     *          instance remains unaffected
      * @throws  RuleNotFoundException if any time unit is not registered
      *          and does also not implement {@link UnitRule.Source}
      * @throws  ArithmeticException in case of numerical overflow
@@ -162,14 +268,28 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
-     * <p>Subtrahiert die angegebene Zeitspanne von der Bezugszeit und
-     * liefert das Ergebnis zur&uuml;ck. </p>
+     * <p>Subtracts given time span from this time point and yields
+     * the result of subtraction. </p>
      *
      * <p>Delegiert an {@link TimeSpan#subtractFrom(TimePoint)}. </p>
      *
      * @param   timeSpan    time span to be subtracted from this instance
-     * @return  result of subtraction as changed copy, this instance
-     *          remains unaffected
+     * @return  result of subtraction as changed copy, this
+     *          instance remains unaffected
+     * @throws  RuleNotFoundException if any time unit is not registered
+     *          and does also not implement {@link UnitRule.Source}
+     * @throws  ArithmeticException in case of numerical overflow
+     * @see     #plus(TimeSpan)
+     */
+    /*[deutsch]
+     * <p>Subtrahiert die angegebene Zeitspanne von der Bezugszeit und
+     * liefert das Subtraktionsergebnis zur&uuml;ck. </p>
+     *
+     * <p>Delegiert an {@link TimeSpan#subtractFrom(TimePoint)}. </p>
+     *
+     * @param   timeSpan    time span to be subtracted from this instance
+     * @return  result of subtraction as changed copy, this
+     *          instance remains unaffected
      * @throws  RuleNotFoundException if any time unit is not registered
      *          and does also not implement {@link UnitRule.Source}
      * @throws  ArithmeticException in case of numerical overflow
@@ -182,6 +302,33 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
+     * <p>Adds given amount in units to this time point and yields the
+     * result of addition. </p>
+     *
+     * <p>Similar to {@link #plus(TimeSpan)} but with the difference
+     * that the timespan is only given in one single time unit. Example
+     * in pseudo-code: </p>
+     *
+     * <ul>
+     *  <li>[2011-05-31].plus(1, &lt;MONTHS&gt;) = [2011-06-30]</li>
+     *  <li>[2011-05-31].plus(4, &lt;DAYS&gt;) = [2011-06-04]</li>
+     *  <li>[2011-06-04].plus(-4, &lt;DAYS&gt;) = [2011-05-31]</li>
+     *  <li>[2010-04-29].plus(397, &lt;DAYS&gt;) = [2011-05-31]</li>
+     *  <li>[2010-04-29].plus(13, &lt;MONTHS&gt;) = [2011-05-29]</li>
+     *  <li>[2010-04-29].plus(-2, &lt;MONTHS&gt;) = [2010-02-28]</li>
+     *  <li>[2010-04-29].plus(1, &lt;YEARS&gt;) = [2011-04-29]</li>
+     * </ul>
+     *
+     * @param   amount      amount to be added (maybe negative)
+     * @param   unit        time unit
+     * @return  result of addition as changed copy, this instance
+     *          remains unaffected
+     * @throws  RuleNotFoundException if the time unit is not registered
+     *          and does also not implement {@link UnitRule.Source}
+     * @throws  ArithmeticException in case of numerical overflow
+     * @see     #plus(TimeSpan)
+     */
+    /*[deutsch]
      * <p>Addiert den angegebenen Betrag der entsprechenden Zeiteinheit
      * zu dieser Bezugszeit und liefert das Additionsergebnis zur&uuml;ck. </p>
      *
@@ -222,6 +369,19 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
+     * <p>Subtracts given amount in units from this time point and
+     * yields the result of subtraction. </p>
+     *
+     * @param   amount      amount to be subtracted (maybe negative)
+     * @param   unit        time unit
+     * @return  result of subtraction as changed copy, this instance
+     *          remains unaffected
+     * @throws  RuleNotFoundException if the time unit is not registered
+     *          and does also not implement {@link UnitRule.Source}
+     * @throws  ArithmeticException in case of numerical overflow
+     * @see     #plus(long, Object) plus(long, U)
+     */
+    /*[deutsch]
      * <p>Subtrahiert den angegebenen Betrag der entsprechenden Zeiteinheit
      * von dieser Bezugszeit und liefert das Ergebnis zur&uuml;ck. </p>
      *
@@ -244,8 +404,18 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
-     * <p>Ermittelt die normalisierte Zeitspanne zwischen dieser Bezugszeit und
-     * dem Endzeitpunkt in der angegebenen Metrik. </p>
+     * <p>Calculates the (most normalized) time span between this time point
+     * and given end time point using the given metric. </p>
+     *
+     * @param   end             end time point
+     * @param   metric          temporal distance metric
+     * @return  difference between this and given end time point
+     *          expressed as time span
+     * @throws  ArithmeticException in case of numerical overflow
+     */
+    /*[deutsch]
+     * <p>Ermittelt die (meist normalisierte) Zeitspanne zwischen dieser
+     * Bezugszeit und dem Endzeitpunkt in der angegebenen Metrik. </p>
      *
      * @param   end             end time point
      * @param   metric          temporal distance metric
@@ -263,6 +433,37 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
+     * <p>Calculates the temporal distance between this time point and
+     * given end time point in only one time unit. </p>
+     *
+     * <p>Similar to {@link #until(TimePoint, TimeMetric)} but with the
+     * difference that the time span is onyl calculated in one time unit
+     * as long-primitive. In many cases a remainder of subtraction will
+     * be left if given unit is not the smallest possible unit. Time points
+     * whose element values differ less than one base unit will be
+     * considered as equal. Examples in pseudo-code: </p>
+     *
+     * <ul>
+     *  <li>[2011-05-31].until([2011-06-04], &lt;MONTHS&gt;) = 0</li>
+     *  <li>[2011-05-31].until([2011-06-04], &lt;DAYS&gt;) = 4</li>
+     *  <li>[2011-06-04].until([2011-05-31], &lt;DAYS&gt;) = -4</li>
+     *  <li>[2010-04-29].until([2011-05-31], &lt;DAYS&gt;) = 397</li>
+     *  <li>[2010-04-29].until([2011-05-31], &lt;MONTHS&gt;) = 13</li>
+     *  <li>[2010-04-29].until([2011-05-31], &lt;YEARS&gt;) = 1</li>
+     *  <li>[2010-05-31].until([2011-05-31], &lt;YEARS&gt;) = 1</li>
+     *  <li>[2010-06-01].until([2011-05-31], &lt;YEARS&gt;) = 0</li>
+     * </ul>
+     *
+     * @param   end     end time point
+     * @param   unit    time unit
+     * @return  difference between this and given end time point
+     *          as count of given time unit
+     * @throws  RuleNotFoundException if the time unit is not registered
+     *          and does also not implement {@link UnitRule.Source}
+     * @throws  ArithmeticException in case of numerical overflow
+     * @see     #until(TimePoint, TimeMetric)
+     */
+    /*[deutsch]
      * <p>Ermittelt den zeitlichen Abstand zwischen dieser Bezugszeit und dem
      * angegebenen Zeitpunkt nur in einer Zeiteinheit zur&uuml;ck. </p>
      *
@@ -271,7 +472,8 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
      * long-Primitive ermittelt wird. Es wird meist ein Subtraktionsrest
      * verbleiben, wenn es sich nicht um die kleinstm&ouml;gliche bzw. genaueste
      * Zeiteinheit handelt. Zeitpunkte, deren Elementwerte sich um weniger als
-     * eine Einheit unterscheiden, gelten als gleich. Beispiele: </p>
+     * eine Einheit unterscheiden, gelten als gleich. Beispiele in
+     * Pseudo-Code: </p>
      *
      * <ul>
      *  <li>[2011-05-31].until([2011-06-04], &lt;MONTHS&gt;) = 0</li>
@@ -303,7 +505,16 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
-     * <p>Bestimmt das Minimum der beiden Argumente. </p>
+     * <p>Determines the minimum of both time points. </p>
+     *
+     * @param   <U> generic type of time units compatible to {@link ChronoUnit}
+     * @param   <T> generic type of self reference
+     * @param   t1      first time point
+     * @param   t2      second time point
+     * @return  minimum of t1 and t2
+     */
+    /*[deutsch]
+     * <p>Bestimmt das Minimum der beiden Zeitpunkte. </p>
      *
      * @param   <U> generic type of time units compatible to {@link ChronoUnit}
      * @param   <T> generic type of self reference
@@ -321,7 +532,16 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
-     * <p>Bestimmt das Maximum der beiden Argumente. </p>
+     * <p>Determines the maximum of both time points. </p>
+     *
+     * @param   <U> generic type of time units compatible to {@link ChronoUnit}
+     * @param   <T> generic type of self reference
+     * @param   t1      first time point
+     * @param   t2      second time point
+     * @return  maximum of t1 and t2
+     */
+    /*[deutsch]
+     * <p>Bestimmt das Maximum der beiden Zeitpunkte. </p>
      *
      * @param   <U> generic type of time units compatible to {@link ChronoUnit}
      * @param   <T> generic type of self reference
@@ -339,6 +559,16 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     }
 
     /**
+     * <p>Compares the whole state of this instance with given object. </p>
+     *
+     * <p>Implementations will usually define their state only
+     * based on the temporal position on the time axis because this
+     * is the most intuitive approach. Exceptions from this rule should
+     * be explicitly documented and reasoned. </p>
+     *
+     * @see     #compareTo(TimePoint)
+     */
+    /*[deutsch]
      * <p>Vergleicht den gesamten Zustand dieser Instanz mit dem des
      * angegebenen Objekts. </p>
      *
@@ -353,6 +583,10 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     public abstract boolean equals(Object obj);
 
     /**
+     * <p>Subclasses must redefine this method corresponding to the
+     * behaviour of {@code equals()}. </p>
+     */
+    /*[deutsch]
      * <p>Subklassen m&uuml;ssen diese Methode passend zum Verhalten
      * von {@code equals()} redefinieren. </p>
      */
@@ -360,6 +594,15 @@ public abstract class TimePoint<U, T extends TimePoint<U, T>>
     public abstract int hashCode();
 
     /**
+     * <p>Provides a complete textual representation of the state of
+     * this time point. </p>
+     *
+     * <p>The textual description often follows the conventions of ISO-8601.
+     * Usually the description starts with the chronological informations
+     * which are coarse-grained and ends with those ones which are 
+     * faine-grained (for example the ISO-notation YYYY-MM-DD). </p>
+     */
+    /*[deutsch]
      * <p>Liefert eine vollst&auml;ndige Beschreibung des Zustands dieses
      * Zeitpunkts. </p>
      *
