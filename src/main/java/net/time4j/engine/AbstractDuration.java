d94a34b83c6a26f41fd6a94458041658ead76864
Meno Hochschild <mhochschild@gmx.de>
2014-06-23 13:32:15 +0200

    english translation of AbstractDuration
    see issue #4

--- src/main/java/net/time4j/engine/AbstractDuration.java
+++ src/main/java/net/time4j/engine/AbstractDuration.java
@@ -27,6 +27,122 @@ import java.util.List;
 
 
 /**
+ * <p>Defines a timespan using the default algorithm of Time4J. </p>
+ *
+ * <p><a name="algorithm"></a>Dependent on the sign of the duration
+ * there are three cases: </p>
+ *
+ * <ol>
+ *  <li>Empty duration =&gt; The method {@code addTo()} just yields
+ *  a given time point unaffected. </li>
+ *  <li>Positive duration =&gt; All contained time units will be added
+ *  in the order from largest to smallest units. Convertible units will
+ *  be consolidated in one step. The new time point is relative to
+ *  given time point argument in the future. </li>
+ *  <li>Negative duration =&gt; All contained time units will be
+ *  subtracted in the reversed order from the smallest to the largest
+ *  units.  Convertible units will be consolidated in one step. The
+ *  new time point is relative to given time point argument in the
+ *  past. </li>
+ * </ol>
+ *
+ * <p>Usually possible element overflows will be truncated such that
+ * the last valid time point will be determined. The rest of the
+ * discussion is about the gregorian calendar system and the addition
+ * of months and days, but is also applicable on other calendar
+ * systems. Examples in pseudo-code: </p>
+ *
+ * <ul>
+ *  <li>[2011-05-31] + [P4D] = [2011-06-04]</li>
+ *  <li>[2011-05-31] + [P9M] = [2012-02-29]</li>
+ *  <li>[2011-05-31] + [-P1M] = [2011-04-30]</li>
+ *  <li>[2011-05-30] + [P1M1D] = [2011-07-01]</li>
+ *  <li>[2011-05-31] + [P1M1D] = [2011-07-01]</li>
+ *  <li>[2011-07-01] + [-P1M1D] = [2011-05-30]</li>
+ *  <li>[2011-05-31] + [-P1Y1M1D] = [2010-04-30]</li>
+ * </ul>
+ *
+ * <p>If the smallest existing time unit is used then following
+ * invariants hold for the addition of a duration and the delta
+ * between two time points. Let t1 and t2 be two time points with
+ * {@code t1 <= t2}: </p>
+ *
+ * <ul><li>FIRST INVARIANCE:
+ *  {@code t1.plus(t1.until(t2)).equals(t2) == true}
+ * </li><li>SECOND INVARIANCE:
+ *  {@code t2.until(t1).equals(t1.until(t2).inverse()) == true}
+ * </li></ul>
+ *
+ * <p><strong>Note:</strong> The THIRD INVARIANCE
+ * {@code t1.plus(t1.until(t2)).minus(t1.until(t2)).equals(t1) == true}
+ * is often INVALID. A counter example where this invariance is
+ * violated is given with following dates:
+ * {t1, t2} = {[2011-05-31], [2011-07-01]}. But if the additional
+ * condition is required that the day of month is never after 28th
+ * of a month then this third invariance can be guaranteed.
+ * Therefore it is recommended to avoid dates near the end of
+ * month in addition. </p>
+ *
+ * <div style="background-color:#E0E0E0;padding:5px;margin:5px;">
+ *
+ * <p><strong>About the mathematical background of specified
+ * algorithm:</strong> Note that the addition is not commutative,
+ * hence the order of addition steps will impact the result. For
+ * example a two-step-addition looks like: </p>
+ *
+ * <ul>
+ *  <li>[2011-05-30] + [P1M] + [P2D] = [2011-07-02]</li>
+ *  <li>[2011-05-30] + [P2D] + [P1M] = [2011-07-01]</li>
+ * </ul>
+ *
+ * <p>In this context it is understandable that the order of
+ * addition steps is dependent on the sign of the duration. If
+ * the addition of a negative duration is interpreted as the
+ * reversal of the addition of a positive duration then following
+ * equivalent relation holds (paying attention to non-commutativity
+ * and given the side conditions to compute the duration without
+ * remainder completely and to consider a minus-operation as
+ * equalizing a plus-operation (with t1-day-of-month &lt;= 28): </p>
+ *
+ * <ul>
+ *  <li>[t1] - [months] - [days] = [t2]</li>
+ *  <li>=&gt; [t1] - [months] - [days] + [days] = [t2] + [days]</li>
+ *  <li>=&gt; [t1] - [months] = [t2] + [days]</li>
+ *  <li>=&gt; [t1] - [months] + [months] = [t2] + [days] + [months]</li>
+ *  <li>=&gt; [t1] = [t2] + [days] + [months] // day-of-month &lt;= 28</li>
+ * </ul>
+ *
+ * <p>The permutation of addition steps is obvious. If Time4J had
+ * tried the alternative to first add the months and then the days
+ * even in case of a negative duration then we would have with
+ * 
+ * <ul>
+ *  <li>t1 = [2013-02-01]</li>
+ *  <li>t2 = [2013-03-31]</li>
+ *  <li>duration = t1.until(t2) = [P1M30D]</li>
+ * </ul>
+ * 
+ * the situation that the mentiioned third invariance would be violated
+ * even if the day of month is the first day of month: t2.minus(P1M30D)
+ * would not yield t1 but [2013-01-29]. Surely, the sign-dependent
+ * execution of addition steps cannot completely guarantee the third
+ * invariance but it can guarantee it at least for all days in original
+ * date until the 28th of month. </p>
+ *
+ * <p>Furthermore the specified algorithm ensures the second invariance
+ * {@code Duration([t1, t2]) = -Duration([t2, t1])} which expresses
+ * a physical property of any duration. The second invariance means
+ * that the sign of a duration can only qualify if the first time point
+ * is before the second time point or other way around. The sign must
+ * not qualify the always positive length of a duration itself however. </p>
+ * </div>
+ *
+ * @author  Meno Hochschild
+ * @see     AbstractMetric
+ * @see     #addTo(TimePoint)
+ * @see     #subtractFrom(TimePoint)
+ */
+/*[deutsch]
  * <p>Definiert eine Zeitspanne unter Verwendung des Standard-Algorithmus
  * von Time4J. </p>
  *
@@ -34,8 +150,8 @@ import java.util.List;
  * gibt es drei F&auml;lle zu betrachten: </p>
  *
  * <ol>
- *  <li>Leere Dauer =&gt; Die Methode liefert einfach nur den
- *  angegebenen Zeitpunkt unver&auml;ndert zur&uuml;ck. </li>
+ *  <li>Leere Dauer =&gt; Die Methode {@code addTo()} liefert einfach nur
+ *  den angegebenen Zeitpunkt unver&auml;ndert zur&uuml;ck. </li>
  *  <li>Positive Dauer =&gt; Alle in der Dauer enthaltenen
  *  Zeiteinheiten werden in der Reihenfolge von den gr&ouml;&szlig;ten zu
  *  den kleinsten bzw. genauesten Einheiten hin addiert. Konvertierbare
@@ -178,6 +294,12 @@ public abstract class AbstractDuration<U extends ChronoUnit>
     }
 
     /**
+     * <p>Creates a copy of this duration with the same amounts and
+     * units but the inversed sign. </p>
+     *
+     * @return  inverted duration
+     */
+    /*[deutsch]
      * <p>Erzeugt die Negation dieser Dauer mit gleichen Betr&auml;gen,
      * aber umgekehrtem Vorzeichen. </p>
      *
@@ -208,6 +330,13 @@ public abstract class AbstractDuration<U extends ChronoUnit>
     }
 
     /**
+     * <p>Yields a canonical representation which optionally starts with
+     * the sign then continues with the letter &quot;P&quot; followed
+     * by a comma-separated sequence of duration items. </p>
+     *
+     * @return  String
+     */
+    /*[deutsch]
      * <p>Liefert eine kanonische Darstellung, die optional mit einem negativen
      * Vorzeichen beginnt, dann mit dem Buchstaben &quot;P&quot; fortsetzt,
      * gefolgt von einer komma-separierten Liste der Dauerelemente. </p>
@@ -252,7 +381,11 @@ public abstract class AbstractDuration<U extends ChronoUnit>
     }
 
     /**
-     * <p>Subtrahiert diese Dauer vom angegebenen Zeitpunkt unter Verwendung
+     * <p>Adds this duration to given time point using the
+     * <a href="#algorithm">default algorithm</a>. </p>
+     */
+    /*[deutsch]
+     * <p>Addiert diese Dauer zum angegebenen Zeitpunkt unter Verwendung
      * des <a href="#algorithm">Standard-Algorithmus</a>. </p>
      */
     @Override
@@ -263,6 +396,10 @@ public abstract class AbstractDuration<U extends ChronoUnit>
     }
 
     /**
+     * <p>Subtracts this duration from given time point using the
+     * <a href="#algorithm">default algorithm</a>. </p>
+     */
+    /*[deutsch]
      * <p>Subtrahiert diese Dauer vom angegebenen Zeitpunkt unter Verwendung
      * des <a href="#algorithm">Standard-Algorithmus</a>. </p>
      */
