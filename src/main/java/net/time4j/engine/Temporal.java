35d1815cbc645ca55b5f224623daf18d5f56c42a
Meno Hochschild <mhochschild@gmx.de>
2014-06-24 14:55:15 +0200

    english translation of Temporal
    see issue #4

--- src/main/java/net/time4j/engine/Temporal.java
+++ src/main/java/net/time4j/engine/Temporal.java
@@ -23,6 +23,13 @@ package net.time4j.engine;
 
 
 /**
+ * <p>Represents an object which can be sorted on a time axis in a
+ * temporal-only way. </p>
+ *
+ * @param   <C> generic temporal type for pure temporal comparison purposes
+ * @author  Meno Hochschild
+ */
+/*[deutsch]
  * <p>Repr&auml;sentiert ein Objekt, das auf einem Zeitstrahl rein zeitlich
  * angeordnet werden kann. </p>
  *
@@ -34,6 +41,13 @@ public interface Temporal<C> {
     //~ Methoden ----------------------------------------------------------
 
     /**
+     * <p>Queries if this object is after given object on a timeline. </p>
+     *
+     * @param   temporal    object this instance is compared to
+     * @return  {@code true} if this instance is temporally after
+     *          {@code temporal} else {@code false}
+     */
+    /*[deutsch]
      * <p>Liegt dieses Objekt zeitlich nach dem angegebenen Argument? </p>
      *
      * @param   temporal    object this instance is compared to
@@ -43,6 +57,13 @@ public interface Temporal<C> {
     boolean isAfter(C temporal);
 
     /**
+     * <p>Queries if this object is before given object on a timeline. </p>
+     *
+     * @param   temporal    object this instance is compared to
+     * @return  {@code true} if this instance is temporally before
+     *          {@code temporal} else {@code false}
+     */
+    /*[deutsch]
      * <p>Liegt dieses Objekt zeitlich vor dem angegebenen Argument? </p>
      *
      * @param   temporal    object this instance is compared to
@@ -52,6 +73,19 @@ public interface Temporal<C> {
     boolean isBefore(C temporal);
 
     /**
+     * <p>Queries if this object and given object have the same position
+     * on the time axis. </p>
+     *
+     * <p>Is equivalent to {@code !isAfter(temporal) && !isBefore(temporal)}.
+     * This method differs from the {@code Object}-method {@code equals()}
+     * such that first the comparison type must be a temporal one and second
+     * that only temporal-only state will be considered. </p>
+     *
+     * @param   temporal    object this instance is compared to
+     * @return  {@code true} if this instance is temporally equal
+     *          to {@code temporal} else {@code false}
+     */
+    /*[deutsch]
      * <p>Sind dieses Objekt und das angegebene Argument zeitlich gleich? </p>
      *
      * <p>Entspricht {@code !isAfter(temporal) && !isBefore(temporal)}. Diese
