9a42a53bc8e7cff11cffae7727085023273f280c
Meno Hochschild <mhochschild@gmx.de>
2014-06-25 13:18:25 +0200

    english translation of ChronoElement
    see issue #4

--- src/main/java/net/time4j/engine/ChronoElement.java
+++ src/main/java/net/time4j/engine/ChronoElement.java
@@ -25,6 +25,29 @@ import java.util.Comparator;
 
 
 /**
+ * <p>Represents a chronological element which refers to a partial value of
+ * the whole temporal value and mainly serves for formatting purposes (as
+ * a carrier of a chronological information). </p>
+ *
+ * <p>Each chronological system knows a set of elements whose values
+ * compose the total temporal value, usually the associated time coordinates
+ * on a time axis. Each element can be associated with a value of type V.
+ * Normally this value is an integer or an enum. Examples are the hour on
+ * a clock or the month of a calendar date. </p>
+ *
+ * <p>The associated values are often defined as continuum without any
+ * gaps as integer within a given value range. However, there is no
+ * guarantee for such a continuum, for example daylight-saving-jumps or
+ * hebrew leap months. </p>
+ *
+ * @param   <V> generic type of element values, usually extending the interface
+ *          {@code java.lang.Comparable} (or it can be converted to any other
+ *          sortable form)
+ * @author  Meno Hochschild
+ * @see     ChronoEntity#get(ChronoElement)
+ * @spec    All implementations must be immutable and serializable.
+ */
+/*[deutsch]
  * <p>Repr&auml;sentiert ein chronologisches Element, das einen Teil eines
  * Zeitwerts referenziert und als Tr&auml;ger einer chronologischen Information
  * in erster Linie Formatierzwecken dient. </p>
@@ -54,6 +77,15 @@ public interface ChronoElement<V>
     //~ Methoden ----------------------------------------------------------
 
     /**
+     * <p>Returns the name which is unique within the context of a given
+     * chronology. </p>
+     *
+     * <p>The name can also serve as resource key together with the name of
+     * a chronology for a localized description. </p>
+     *
+     * @return  name of element, unique within a chronology
+     */
+    /*[deutsch]
      * <p>Liefert den innerhalb einer Chronologie eindeutigen Namen. </p>
      *
      * <p>Zusammen mit dem Namen der zugeh&ouml;rigen Chronologie kann
@@ -65,6 +97,11 @@ public interface ChronoElement<V>
     String name();
 
     /**
+     * <p>Yields the reified value type. </p>
+     *
+     * @return  type of associated values
+     */
+    /*[deutsch]
      * <p>Liefert die Wertklasse. </p>
      *
      * @return  type of associated values
@@ -72,6 +109,18 @@ public interface ChronoElement<V>
     Class<V> getType();
 
     /**
+     * <p>Defines the default format symbol which is used in format patterns
+     * to refer to this element. </p>
+     *
+     * <p>In most cases the symbol should closely match the symbol-mapping
+     * as defined by the CLDR-standard of unicode-organization. Is the
+     * element not designed for formatting using patterns then this method
+     * just yields the ASCII-char &quot;\u0000&quot; (Codepoint 0). </p>
+     *
+     * @return  format symbol as char
+     * @see     FormattableElement
+     */
+    /*[deutsch]
      * <p>Definiert das Standard-Formatsymbol, mit dem diese Instanz in
      * Formatmustern referenziert wird. </p>
      *
@@ -86,6 +135,31 @@ public interface ChronoElement<V>
     char getSymbol();
 
     /**
+     * <p>Applies an element-orientated sorting of any chronological 
+     * entities. </p>
+     *
+     * <p>The value type V is usually a subtype of the interface
+     * {@code Comparable} so that this method will usually be based on
+     * the expression {@code o1.get(this).compareTo(o2.get(this))}. In
+     * other words, this method compares the element values of given
+     * chronological entities. It is even permitted to compare entities
+     * of different chronological types as long as the entities both
+     * support this element. </p>
+     *
+     * <p>It should be noted however that a element value comparison does
+     * often not induce any temporal (complete) order. Counter examples
+     * are the year of gregorian BC-era (reversed temporal order before
+     * Jesu birth) or the clock display of hours (12 is indeed the begin
+     * at midnight). </p>
+     *
+     * @param   o1  the first object to be compared
+     * @param   o2  the second object to be compared
+     * @return  a negative integer, zero, or a positive integer as the first
+     *          argument is less than, equal to, or greater than the second
+     * @throws  ChronoException if this element is not registered in any entity
+     *          and/or if no element rule exists to extract the element value
+     */
+    /*[deutsch]
      * <p>F&uuml;hrt eine elementorientierte Sortierung von beliebigen
      * chronologischen Objekten bez&uuml;glich dieses Elements ein . </p>
      *
@@ -96,7 +170,8 @@ public interface ChronoElement<V>
      * die Werte bez&uuml;glich dieses Elements verglichen. Es d&uuml;rfen
      * anders als im {@code Comparable}-Interface m&ouml;glich hierbei auch
      * Objekte verschiedener chronologischer Typen miteinander verglichen
-     * werden. </p>
+     * werden solange die zu vergleichenden Objekte dieses Element
+     * unterst&uuml;tzen. </p>
      *
      * <p>Es ist zu betonen, da&szlig; ein Elementwertvergleich im allgemeinen
      * keine (vollst&auml;ndige) zeitliche Sortierung indiziert. Gegenbeispiele
@@ -118,6 +193,20 @@ public interface ChronoElement<V>
     );
 
     /**
+     * <p>Returns the default minimum of this element which is not dependent
+     * on the chronological context. </p>
+     *
+     * <p>Note: This minimum does not necessarily define a minimum for all
+     * possible circumstances but only the default minimum under normal
+     * conditions such that the default minimum always exists and can be
+     * used as prototypical value. An example is the start of day which
+     * is usually midnight in ISO-8601 and can only deviate in specialized
+     * timezone context. </p>
+     *
+     * @return  default minimum value of element
+     * @see     #getDefaultMaximum()
+     */
+    /*[deutsch]
      * <p>Gibt das &uuml;bliche chronologieunabh&auml;ngige Minimum des
      * assoziierten Elementwerts zur&uuml;ck. </p>
      *
@@ -134,14 +223,28 @@ public interface ChronoElement<V>
     V getDefaultMinimum();
 
     /**
+     * <p>Returns the default maximum of this element which is not dependent
+     * on the chronological context. </p>
+     *
+     * <p>Note: This maximum does not necessarily define a maximum for all
+     * possible circumstances but only the default maximum under normal
+     * conditions. An example is the second of minute whose default maximum
+     * is {@code 59} while the largest maximum can be {@code 60} in context
+     * of UTC-leapseconds. </p>
+     *
+     * @return  default maximum value of element
+     * @see     #getDefaultMinimum()
+     */
+    /*[deutsch]
      * <p>Gibt das &uuml;bliche chronologieunabh&auml;ngige Maximum des
      * assoziierten Elementwerts zur&uuml;ck. </p>
      *
      * <p>Hinweis: Dieses Maximum definiert keineswegs eine obere Schranke
      * f&uuml;r alle g&uuml;ltigen Elementwerte, sondern nur das Standardmaximum
-     * unter chronologischen Normalbedingungen . Ein Beispiel sind Sekunden,
-     * deren Standardmaximum {@code 59} ist, w&auml;hrend das gr&ouml;&szlig;te
-     * Maximum bedingt durch UTC-Schaltsekunden {@code 60} sein kann. </p>
+     * unter chronologischen Normalbedingungen . Ein Beispiel ist das
+     * Element SECOND_OF_MINUTE, dessen Standardmaximum {@code 59} ist,
+     * w&auml;hrend das gr&ouml;&szlig;te Maximum bedingt durch
+     * UTC-Schaltsekunden {@code 60} sein kann. </p>
      *
      * @return  default maximum value of element
      * @see     #getDefaultMinimum()
@@ -149,6 +252,12 @@ public interface ChronoElement<V>
     V getDefaultMaximum();
 
     /**
+     * <p>Queries if this element represents a calendar date element. </p>
+     *
+     * @return  boolean
+     * @see     #isTimeElement()
+     */
+    /*[deutsch]
      * <p>Ist dieses Element eine Teilkomponente, die ein Datumselement
      * darstellt? </p>
      *
@@ -158,6 +267,12 @@ public interface ChronoElement<V>
     boolean isDateElement();
 
     /**
+     * <p>Queries if this element represents a wall time  element. </p>
+     *
+     * @return  boolean
+     * @see     #isDateElement()
+     */
+    /*[deutsch]
      * <p>Ist dieses Element eine Teilkomponente, die ein Uhrzeitelement
      * darstellt? </p>
      *
@@ -167,6 +282,14 @@ public interface ChronoElement<V>
     boolean isTimeElement();
 
     /**
+     * <p>Queries if setting of element values is performed in a lenient
+     * way. </p>
+     *
+     * @return  {@code true} if lenient else {@code false} (standard)
+     * @see     ElementRule#withValue(Object, Object, boolean)
+     *          ElementRule.withValue(T, V, boolean)
+     */
+    /*[deutsch]
      * <p>Soll das Setzen von Werten fehlertolerant ausgef&uuml;hrt werden? </p>
      *
      * @return  {@code true} if lenient else {@code false} (standard)
