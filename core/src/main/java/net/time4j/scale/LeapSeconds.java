f6f53dd4d977dfd2d87dd737628e2d31b7edda3e
Meno Hochschild <mhochschild@gmx.de>
2014-11-08 14:48:39 +0100

    rename SPI-interface for leapseconds
    see issue #160

--- core/src/main/java/net/time4j/scale/LeapSeconds.java
+++ core/src/main/java/net/time4j/scale/LeapSeconds.java
@@ -181,15 +181,16 @@ public final class LeapSeconds
             ClassLoader cl = Thread.currentThread().getContextClassLoader();
 
             if (cl == null) {
-                cl = Provider.class.getClassLoader();
+                cl = LeapSecondProvider.class.getClassLoader();
             }
 
-            ServiceLoader<Provider> sl = ServiceLoader.load(Provider.class, cl);
-            Provider loaded = null;
+            ServiceLoader<LeapSecondProvider> sl =
+                ServiceLoader.load(LeapSecondProvider.class, cl);
+            LeapSecondProvider loaded = null;
             int leapCount = 0;
 
             // Provider mit den meisten Schaltsekunden wählen
-            for (Provider temp : sl) {
+            for (LeapSecondProvider temp : sl) {
                 int currentCount = temp.getLeapSecondTable().size();
 
                 if (currentCount > leapCount) {
@@ -437,14 +438,14 @@ public final class LeapSeconds
      *
      * @return  {@code true} if negative leap seconds are supported
      *          else {@code false}
-     * @see     Provider#supportsNegativeLS()
+     * @see     LeapSecondProvider#supportsNegativeLS()
      */
     /*[deutsch]
      * <p>Werden auch negative Schaltsekunden unterst&uuml;tzt? </p>
      *
      * @return  {@code true} if negative leap seconds are supported
      *          else {@code false}
-     * @see     Provider#supportsNegativeLS()
+     * @see     LeapSecondProvider#supportsNegativeLS()
      */
     public boolean supportsNegativeLS() {
 
@@ -914,85 +915,6 @@ public final class LeapSeconds
 
     }
 
-    //~ Innere Interfaces -------------------------------------------------
-
-    /**
-     * <p>This <strong>SPI-interface</strong> describes when
-     * UTC-leapseconds were introduced. </p>
-     *
-     * <p>Will be evaluated during loading of the class {@code LeapSeconds}.
-     * If any implementation defines no leapseconds then Time4J assumes
-     * that leapseconds will generally not be active, effectively resulting
-     * in POSIX-time instead of UTC. </p>
-     *
-     * @author  Meno Hochschild
-     * @spec    All implementations must have a public no-arg constructor.
-     */
-    /*[deutsch]
-     * <p>Dieses <strong>SPI-Interface</strong> beschreibt, wann
-     * UTC-Schaltsekunden eingef&uuml;hrt worden sind. </p>
-     *
-     * <p>Wird beim Laden der Klasse {@code LeapSeconds} ausgewertet. Wenn
-     * eine Implementierung zum Beispiel per eigener Konfiguration keine
-     * Schaltsekunden definiert, so wird angenommen, da&szlig; generell
-     * keine Schaltsekunden verwendet werden sollen, also die POSIX-Zeit
-     * statt UTC. </p>
-     *
-     * @author  Meno Hochschild
-     * @spec    All implementations must have a public no-arg constructor.
-     */
-    public interface Provider {
-
-        //~ Methoden ------------------------------------------------------
-
-        /**
-         * <p>Yields all UTC-leapseconds with date and sign. </p>
-         *
-         * <p>The switch-over day in the UTC-timezone is considered as
-         * map key. The associated value is denotes the sign of the
-         * leapsecond. Is the value {@code +1} then it is a positive
-         * leapsecond. Is the value {@code -1} then it is a negative
-         * leapsecond. Other values are not supported. </p>
-         *
-         * @return  map from leap second event day to sign of leap second
-         */
-        /*[deutsch]
-         * <p>Liefert alle UTC-Schaltsekunden mit Datum und Vorzeichen. </p>
-         *
-         * <p>Als Schl&uuml;ssel wird der Umstellungstag in der UTC-Zeitzone
-         * genommen. Der zugeordnete Wert bezeichnet das Vorzeichen der
-         * Schaltsekunde. Ist der Wert {@code +1}, dann handelt es sich um
-         * eine positive Schaltsekunde. Ist der Wert {@code -1}, dann wird
-         * eine negative Schaltsekunde angenommen. Andere Werte werden nicht
-         * unterst&uuml;tzt. </p>
-         *
-         * @return  map from leap second event day to sign of leap second
-         */
-        Map<GregorianDate, Integer> getLeapSecondTable();
-
-        /**
-         * <p>Queries if negative leapseconds are supported. </p>
-         *
-         * <p>Until now there has never been any negative leapseconds.
-         * As long as this is the case a {@code Provider} is allowed to
-         * return {@code false} in order to improve the performance. </p>
-         *
-         * @return  {@code true} if supported else {@code false}
-         */
-        /*[deutsch]
-         * <p>Werden auch negative Schaltsekunden unterst&uuml;tzt? </p>
-         *
-         * <p>Bis jetzt hat es real noch nie negative Schaltsekunden gegeben.
-         * Solange das der Fall ist, darf ein {@code Provider} aus
-         * Gr&uuml;nden der besseren Performance hier {@code false}
-         * zur&uuml;ckgeben. </p>
-         *
-         * @return  {@code true} if supported else {@code false}
-         */
-        boolean supportsNegativeLS();
-
-    }
-
     //~ Innere Klassen ----------------------------------------------------
 
     private static class IsoDate
@@ -1251,7 +1173,7 @@ public final class LeapSeconds
     }
 
     private static class DefaultLeapSecondService
-        implements Provider {
+        implements LeapSecondProvider {
 
         //~ Instanzvariablen ----------------------------------------------
 
@@ -1273,7 +1195,7 @@ public final class LeapSeconds
             }
 
             if (is == null) {
-                cl = LeapSeconds.Provider.class.getClassLoader();
+                cl = LeapSecondProvider.class.getClassLoader();
                 is = cl.getResourceAsStream(name);
             }
 
