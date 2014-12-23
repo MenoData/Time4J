2c590ec2b5850d203f9497f8a182d0320e7185b0
Meno Hochschild <mhochschild@gmx.de>
2014-12-23 16:45:17 +0100

    test adjustment
    see issue #39

--- core/src/test/java/net/time4j/scale/NextLeapSecondTest.java
+++ core/src/test/java/net/time4j/scale/NextLeapSecondTest.java
@@ -12,33 +12,42 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-// TODO: needs adjustment if any new leap second is announced
 @RunWith(JUnit4.class)
 public class NextLeapSecondTest {
 
     @Test
-    public void nextLS() {
-        if (LeapSeconds.getInstance().getCount() <= 25) {
-            assertThat(
-                Moment.nextLeapSecond(),
-                nullValue());
+    public void nextLS2012() {
+        Moment ref = PlainDate.of(2012, 1, 1).atTime(0, 0).atUTC();
+        if (LeapSeconds.getInstance().isEnabled()) {
+            Moment expected = 
+                PlainDate.of(2012, 6, 30).atTime(23, 59, 59).atUTC().plus(1, SI.SECONDS);
+            assertThat(ref.with(Moment.nextLeapSecond()), is(expected));
         } else {
-            System.out.println("Warning: NextLeapSecondTest not up-to-date.");
+            assertThat(ref.with(Moment.nextLeapSecond()), is(nullValue()));
         }
     }
 
     @Test
     public void nextLSIfKnown() {
-        if (LeapSeconds.getInstance().getCount() <= 25) {
+        if (LeapSeconds.getInstance().isEnabled()) {
             LeapSeconds.getInstance().registerPositiveLS(2115, 12, 31);
+            Moment ref = PlainDate.of(2100, 1, 1).atTime(0, 0).atUTC();
             assertThat(
-                Moment.nextLeapSecond(),
+                ref.with(Moment.nextLeapSecond()),
                 is(
                     PlainTimestamp.of(2115, 12, 31, 23, 59, 59).atUTC()
                     .plus(1, SI.SECONDS)));
-        } else {
-            System.out.println("Warning: NextLeapSecondTest not up-to-date.");
-        }
+         } else {
+            assertThat(ref.with(Moment.nextLeapSecond()), is(nullValue()));
+       }
+    }
+
+    @Test
+    public void nextLSIfUnknown() {
+        Moment ref = PlainDate.of(2100, 1, 1).atTime(0, 0).atUTC();
+        assertThat(
+            ref.with(Moment.nextLeapSecond()),
+            is(nullValue()));
     }
 
 }
