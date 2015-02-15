e53a06bb452c4911c9e31c715378640ed1ab3273
Meno Hochschild <mhochschild@gmx.de>
2015-02-15 02:16:13 +0100

    update of package infos in javadoc

--- i18n/src/main/java/net/time4j/i18n/package-info.java
+++ i18n/src/main/java/net/time4j/i18n/package-info.java
@@ -1,8 +1,14 @@
 
 /**
  * <p>Support for accessing localized resources. </p>
+ *
+ * <p><strong>This package belongs to module &quot;i18n&quot;
+ * and is not part of the core of Time4J.</strong></p>
  */
 /*[deutsch]
  * <p>Unterst&uuml;tzung des Zugriffs auf lokalisierte Ressourcen. </p>
+ *
+ * <p><strong>Dieses Paket geh&ouml;rt zum &quot;i18n&quot;-Modul
+ * und ist kein Teil des Kerns von Time4J.</strong></p>
  */
 package net.time4j.i18n;
