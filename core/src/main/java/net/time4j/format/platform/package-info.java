4ac2372118794d1fd3db2b8c258c64330c8f98c0
Meno Hochschild <mhochschild@gmx.de>
2015-04-17 07:32:10 +0200

    delete extra package net.time4j.format.platform

--- core/src/main/java/net/time4j/format/platform/package-info.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * <p>Realizes the format engine on the actual platform. </p>
- */
-/*[deutsch]
- * <p>Realisiert die {@code FormatEngine} der aktuellen Plattform. </p>
- */
-package net.time4j.format.platform;
