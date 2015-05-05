1b83173cb94310b2c68bf5a851c405313519820a
Meno Hochschild <mhochschild@gmx.de>
2015-05-05 07:24:14 +0200

    add package-info

---
+++ olson/src/main/java/net/time4j/tz/threeten/package-info.java
@@ -0,0 +1,20 @@
+
+/**
+ * <p>Encapsulates the zi-repository of Sun/Oracle for
+ * Java-versions 8 or later. </p>
+ *
+ * <p><strong>This package belongs to module &quot;olson&quot;
+ * and is not part of the core of Time4J.</strong></p>
+ *
+ * @doctags.exclude
+ */
+/*[deutsch]
+ * <p>Kapselt die Zeitzonendatenbank von Sun/Oracle in den
+ * Java-Versionen 8 oder sp&auml;ter. </p>
+ *
+ * <p><strong>Dieses Paket geh&ouml;rt zum &quot;olson&quot;-Modul
+ * und ist kein Teil des Kerns von Time4J.</strong></p>
+ *
+ * @doctags.exclude
+ */
+package net.time4j.tz.threeten;
