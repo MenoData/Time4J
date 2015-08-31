c9bb02914851b2782bb05a5fc79d0198f5aad2a2
Meno Hochschild <mhochschild@gmx.de>
2015-08-31 06:26:10 +0200

    small javadoc fix

--- core/src/main/java/net/time4j/PrettyTime.java
+++ core/src/main/java/net/time4j/PrettyTime.java
@@ -1028,7 +1028,7 @@ public final class PrettyTime {
      * &quot;tomorrow&quot; is possible. Example: </p>
      *
      * <pre>
-     *      TimeSource&lt;?&gt; clock = () -> PlainTimestamp.of(2015, 8, 1, 10, 24, 5).atUTC();
+     *      TimeSource&lt;?&gt; clock = () -&gt; PlainTimestamp.of(2015, 8, 1, 10, 24, 5).atUTC();
      *      String durationInDays =
      *          PrettyTime.of(Locale.GERMAN).withReferenceClock(clock).printRelative(
      *              PlainTimestamp.of(2015, 8, 1, 17, 0).atUTC(),
