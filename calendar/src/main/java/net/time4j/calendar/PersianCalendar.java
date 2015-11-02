35e61c7544b52885773dccef553b6d5b01874941
Meno Hochschild <mhochschild@gmx.de>
2015-11-02 05:05:50 +0100

    small javadoc fix (semicolon added)

--- calendar/src/main/java/net/time4j/calendar/PersianCalendar.java
+++ calendar/src/main/java/net/time4j/calendar/PersianCalendar.java
@@ -103,8 +103,8 @@ import java.util.Locale;
  *
  * <pre>
  *     ChronoFormatter&lt;PersianCalendar&gt; formatter =
- *       ChronoFormatter.setUp(PersianCalendar.class, new Locale(&quot;fa&quot;))
- *       .addPattern(&quot;EEE, d. MMMM yy&quot;, PatternType.NON_ISO_DATE).build()
+ *       ChronoFormatter.setUp(PersianCalendar.axis(), new Locale(&quot;fa&quot;))
+ *       .addPattern(&quot;EEE, d. MMMM yy&quot;, PatternType.NON_ISO_DATE).build();
  *     PlainDate today = SystemClock.inLocalView().today();
  *     PersianCalendar jalali = today.transform(PersianCalendar.class);
  *     System.out.println(formatter.format(jalali));
@@ -147,8 +147,8 @@ import java.util.Locale;
  *
  * <pre>
  *     ChronoFormatter&lt;PersianCalendar&gt; formatter =
- *       ChronoFormatter.setUp(PersianCalendar.class, new Locale(&quot;fa&quot;))
- *       .addPattern(&quot;EEE, d. MMMM yy&quot;, PatternType.NON_ISO_DATE).build()
+ *       ChronoFormatter.setUp(PersianCalendar.axis(), new Locale(&quot;fa&quot;))
+ *       .addPattern(&quot;EEE, d. MMMM yy&quot;, PatternType.NON_ISO_DATE).build();
  *     PlainDate today = SystemClock.inLocalView().today();
  *     PersianCalendar jalali = today.transform(PersianCalendar.class);
  *     System.out.println(formatter.format(jalali));
