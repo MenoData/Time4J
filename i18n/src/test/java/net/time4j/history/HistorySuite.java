63e2671286f27068acfa1fc9cb58229130ff50c3
Meno Hochschild <mhochschild@gmx.de>
2016-02-26 01:03:13 +0100

    computus algorithm
    see issue #460

--- i18n/src/test/java/net/time4j/history/HistorySuite.java
+++ i18n/src/test/java/net/time4j/history/HistorySuite.java
@@ -8,6 +8,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses(
     {
+        ComputusTest.class,
         DayOfYearTest.class,
         EraFormatTest.class,
         EraNameTest.class,
