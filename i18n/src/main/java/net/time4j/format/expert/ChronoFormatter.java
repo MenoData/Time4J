8063e46a18bbde5c030708df575b516dd5e2c493
Meno Hochschild <mhochschild@gmx.de>
2016-07-25 05:28:01 +0200

    refactoring of Iso8601Format
    see issue #520

--- i18n/src/main/java/net/time4j/format/expert/ChronoFormatter.java
+++ i18n/src/main/java/net/time4j/format/expert/ChronoFormatter.java
@@ -868,8 +868,7 @@ public final class ChronoFormatter<T extends ChronoEntity<T>>
      * @param   text        text to be parsed
      * @param   status      parser information (always as new instance)
      * @return  result or {@code null} if parsing does not work
-     * @throws  IndexOutOfBoundsException if the start position is at end of
-     *          text or even behind
+     * @throws  IndexOutOfBoundsException if the start position is at end of text or even behind
      */
     /*[deutsch]
      * <p>Interpretiert den angegebenen Text ab der angegebenen Position im
@@ -880,8 +879,7 @@ public final class ChronoFormatter<T extends ChronoEntity<T>>
      * @param   text        text to be parsed
      * @param   status      parser information (always as new instance)
      * @return  result or {@code null} if parsing does not work
-     * @throws  IndexOutOfBoundsException if the start position is at end of
-     *          text or even behind
+     * @throws  IndexOutOfBoundsException if the start position is at end of text or even behind
      */
     public T parse(
         CharSequence text,
