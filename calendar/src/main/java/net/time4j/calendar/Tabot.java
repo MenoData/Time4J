7a32b985c81ba941eae155b6b44c899ad1ebdb15
Meno Hochschild <mhochschild@gmx.de>
2016-09-06 06:45:31 +0200

    renaming of calendar resources

--- calendar/src/main/java/net/time4j/calendar/Tabot.java
+++ calendar/src/main/java/net/time4j/calendar/Tabot.java
@@ -221,7 +221,7 @@ public final class Tabot
     ) {
 
         return ResourceBundle.getBundle(
-            "calendar/ethiopic",
+            "names/ethiopic",
             locale,
             GenericTextProviderSPI.class.getClassLoader(),
             control);
