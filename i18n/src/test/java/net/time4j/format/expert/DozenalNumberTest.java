a25cb557cac06452dc4ac731450247936c69cbcf
Meno Hochschild <mhochschild@gmx.de>
2017-01-03 18:35:51 +0100

    add dozenal numbering system
    see issue #596

---
+++ i18n/src/test/java/net/time4j/format/expert/DozenalNumberTest.java
@@ -0,0 +1,69 @@
+package net.time4j.format.expert;
+
+import net.time4j.PlainDate;
+import net.time4j.TemporalType;
+import net.time4j.format.Attributes;
+import net.time4j.format.NumberSystem;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+@RunWith(JUnit4.class)
+public class DozenalNumberTest {
+
+    @Test
+    public void toNumeral() {
+        assertThat(
+            NumberSystem.DOZENAL.toNumeral(22),
+            is("1\u218A"));
+        assertThat(
+            NumberSystem.DOZENAL.toNumeral(35),
+            is("2\u218B"));
+    }
+
+    @Test
+    public void toInteger() {
+        assertThat(
+            NumberSystem.DOZENAL.toInteger("1\u218A"),
+            is(22));
+        assertThat(
+            NumberSystem.DOZENAL.toInteger("2\u218B"),
+            is(35));
+    }
+
+    @Test
+    public void printDate() {
+        ChronoFormatter<PlainDate> f =
+            ChronoFormatter.setUp(PlainDate.axis(), Locale.ROOT)
+                .addFixedInteger(PlainDate.YEAR, 4)
+                .addLiteral('-')
+                .padNext(2)
+                .addInteger(PlainDate.MONTH_AS_NUMBER, 1, 2)
+                .addLiteral('-')
+                .padNext(2)
+                .addInteger(PlainDate.DAY_OF_MONTH, 1, 2)
+                .build()
+                .with(Attributes.NUMBER_SYSTEM, NumberSystem.DOZENAL)
+                .with(Attributes.PAD_CHAR, '0');
+        assertThat(
+            f.format(PlainDate.of(2017, 10, 11)),
+            is("1201-0\u218A-0\u218B"));
+
+        ChronoFormatter<LocalDate> f2 =
+            ChronoFormatter.ofPattern(
+                "yyyy-ppM-ppd", PatternType.THREETEN, Locale.ROOT, PlainDate.axis(TemporalType.LOCAL_DATE))
+                .with(Attributes.NUMBER_SYSTEM, NumberSystem.DOZENAL)
+                .with(Attributes.PAD_CHAR, '0');
+        assertThat(
+            f2.format(LocalDate.of(2017, 10, 11)),
+            is("1201-0\u218A-0\u218B"));
+    }
+
+}
