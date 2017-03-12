3bd124474ef7c0b9d08cea6feab2cf01414a49e6
Meno Hochschild <mhochschild@gmx.de>
2017-03-12 02:59:37 +0100

    deltaT-calculation
    see issue #93

--- core/src/main/java/net/time4j/scale/TimeScale.java
+++ core/src/main/java/net/time4j/scale/TimeScale.java
@@ -267,4 +267,142 @@ public enum TimeScale {
 //     */
 //    UT1;
 
+    /**
+     * <p>Estimates the delta between TT and UT1 in decimal seconds depending on given year and month. </p>
+     *
+     * <p>The estimation is based
+     * on a <a href="https://eclipse.gsfc.nasa.gov/SEcat5/deltatpoly.html">polynomial expression of the NASA</a>. </p>
+     *
+     * @param   year    gregorian/julian year from -2000 until +3000
+     * @param   month   gregorian/julian month in range 1-12
+     * @return  estimated difference deltaT = TT - UT1 in seconds
+     * @throws  IllegalArgumentException if any parameter is out of range
+     * @since   3.30/4.26
+     */
+    /*[deutsch]
+     * <p>Liefert eine Sch&auml;tzung der Differenz zwischen TT und UT1 in dezimalen Sekunden
+     * abh&auml;ngig von den angegebenen Parametern Jahr und Monat. </p>
+     *
+     * <p>Die Sch&auml;tzung basiert auf einem
+     * <a href="https://eclipse.gsfc.nasa.gov/SEcat5/deltatpoly.html">Polynom-Ausdruck der NASA</a>. </p>
+     *
+     * @param   year    gregorian/julian year from -2000 until +3000
+     * @param   month   gregorian/julian month in range 1-12
+     * @return  estimated difference deltaT = TT - UT1 in seconds
+     * @throws  IllegalArgumentException if any parameter is out of range
+     * @since   3.30/4.26
+     */
+    public static double deltaT(
+        int year,
+        int month
+    ) {
+
+        if ((year < -2000) || (year > 3000)) {
+            throw new IllegalArgumentException("Year out of range: " + year);
+        } else if ((month < 1) || (month > 12)) {
+            throw new IllegalArgumentException("Month out of range: " + month);
+        }
+
+        double y = year + (month - 0.5) / 12;
+
+        if (year > 2150) {
+            double u = (y - 1820) / 100;
+            return -20 + 32 * Math.pow(u, 2);
+        } else if (year >= 2050) {
+            double u = (y - 1820) / 100;
+            return -20 + 32 * Math.pow(u, 2) - 0.5628 * (2150 - y);
+        } else if (year >= 2005) {
+            double t = y - 2000;
+            return 62.92
+                + 0.32217 * t
+                + 0.005589 * Math.pow(t, 2);
+        } else if (year >= 1986) {
+            double t = y - 2000;
+            return 63.86
+                + 0.3345 * t
+                - 0.060374 * Math.pow(t, 2)
+                + 0.0017275 * Math.pow(t, 3)
+                + 0.000651814 * Math.pow(t, 4)
+                + 0.00002373599 * Math.pow(t, 5);
+        } else if (year >= 1961) {
+            double t = y - 1975;
+            return 45.45
+                + 1.067 * t
+                - Math.pow(t, 2) / 260
+                - Math.pow(t, 3) / 718;
+        } else if (year >= 1941) {
+            double t = y - 1950;
+            return 29.07
+                + 0.407 * t
+                - Math.pow(t, 2) / 233
+                + Math.pow(t, 3) / 2547;
+        } else if (year >= 1920) {
+            double t = y - 1920;
+            return 21.20
+                + 0.84493 * t
+                - 0.076100 * Math.pow(t, 2)
+                + 0.0020936 * Math.pow(t, 3);
+        } else if (year >= 1900) {
+            double t = y - 1900;
+            return -2.79
+                + 1.494119 * t
+                - 0.0598939 * Math.pow(t, 2)
+                + 0.0061966 * Math.pow(t, 3)
+                - 0.000197 * Math.pow(t, 4);
+        } else if (year >= 1860) {
+            double t = y - 1860;
+            return 7.62
+                + 0.5737 * t
+                - 0.251754 * Math.pow(t, 2)
+                + 0.01680668 * Math.pow(t, 3)
+                -0.0004473624 * Math.pow(t, 4)
+                + Math.pow(t, 5) / 233174;
+        } else if (year >= 1800) {
+            double t = y - 1800;
+            return 13.72
+                - 0.332447 * t
+                + 0.0068612 * Math.pow(t, 2)
+                + 0.0041116 * Math.pow(t, 3)
+                - 0.00037436 * Math.pow(t, 4)
+                + 0.0000121272 * Math.pow(t, 5)
+                - 0.0000001699 * Math.pow(t, 6)
+                + 0.000000000875 * Math.pow(t, 7);
+        } else if (year >= 1700) {
+            double t = y - 1700;
+            return 8.83
+                + 0.1603 * t
+                - 0.0059285 * Math.pow(t, 2)
+                + 0.00013336 * Math.pow(t, 3)
+                - Math.pow(t, 4) / 1174000;
+        } else if (year >= 1600) {
+            double t = y - 1600;
+            return 120
+                - 0.9808 * t
+                - 0.01532 * Math.pow(t, 2)
+                + Math.pow(t, 3) / 7129;
+        } else if (year >= 500) {
+            double u = (y - 1000) / 100;
+            return 1574.2
+                - 556.01 * u
+                + 71.23472 * Math.pow(u, 2)
+                + 0.319781 * Math.pow(u, 3)
+                - 0.8503463 * Math.pow(u, 4)
+                - 0.005050998 * Math.pow(u, 5)
+                + 0.0083572073 * Math.pow(u, 6);
+        } else if (year >= -500) {
+            double u = y / 100;
+            return 10583.6
+                - 1014.41 * u
+                + 33.78311 * Math.pow(u, 2)
+                - 5.952053 * Math.pow(u, 3)
+                - 0.1798452 * Math.pow(u, 4)
+                + 0.022174192 * Math.pow(u, 5)
+                + 0.0090316521 * Math.pow(u, 6);
+        } else {
+            double u = (y - 1820) / 100;
+            return -20 + 32 * Math.pow(u, 2);
+        }
+
+    }
+
 }
