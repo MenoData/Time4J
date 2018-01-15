3639ce0d107dc3182be653230fce25874395492a
Meno Hochschild <mhochschild@gmx.de>
2018-01-15 03:07:11 +0100

    small refactoring

--- calendar/src/main/java/net/time4j/calendar/EastAsianME.java
+++ calendar/src/main/java/net/time4j/calendar/EastAsianME.java
@@ -24,12 +24,8 @@ package net.time4j.calendar;
 import net.time4j.engine.AttributeQuery;
 import net.time4j.engine.ChronoDisplay;
 import net.time4j.engine.ChronoException;
-import net.time4j.format.Attributes;
 import net.time4j.format.CalendarText;
-import net.time4j.format.Leniency;
-import net.time4j.format.NumberSystem;
 import net.time4j.format.TextElement;
-import net.time4j.format.internal.DualFormatElement;
 
 import java.io.IOException;
 import java.io.ObjectStreamException;
@@ -51,7 +47,7 @@ class EastAsianME
 
     static final EastAsianME SINGLETON_EA = new EastAsianME();
 
-    //private static final long serialVersionUID = -2978966174642315851L;
+    private static final long serialVersionUID = -5874268477318061153L;
 
     //~ Methoden ------------------------------------------------------
 
@@ -147,18 +143,7 @@ class EastAsianME
         AttributeQuery attributes
     ) throws IOException, ChronoException {
 
-        Locale loc = attributes.get(Attributes.LANGUAGE, Locale.ROOT);
-        int count = attributes.get(DualFormatElement.COUNT_OF_PATTERN_SYMBOLS, Integer.valueOf(0)).intValue();
-        EastAsianMonth eam = context.get(this);
-        int num = eam.getNumber();
-
-        if (eam.isLeap()) {
-            char defaultLI =
-                CalendarText.getInstance("generic", loc).getTextForms().get("leap-month").charAt(0);
-            buffer.append(attributes.get(EastAsianMonth.LEAP_MONTH_INDICATOR, defaultLI));
-        }
-        NumberSystem numsys = attributes.get(Attributes.NUMBER_SYSTEM, NumberSystem.ARABIC);
-        buffer.append(numsys.toNumeral(num)); // no padding in lunisolar case
+        throw new AbstractMethodError("Not yet available.");
 
     }
 
@@ -169,72 +154,7 @@ class EastAsianME
         AttributeQuery attributes
     ) {
 
-        Locale loc = attributes.get(Attributes.LANGUAGE, Locale.ROOT);
-        int count = attributes.get(DualFormatElement.COUNT_OF_PATTERN_SYMBOLS, Integer.valueOf(0)).intValue();
-        int start = status.getIndex();
-
-        char defaultLI =
-            CalendarText.getInstance("generic", loc).getTextForms().get("leap-month").charAt(0);
-        char li = attributes.get(EastAsianMonth.LEAP_MONTH_INDICATOR, defaultLI).charValue();
-        int pos = start;
-        boolean leap = false;
-
-        if (text.charAt(pos) == li) {
-            leap = true;
-            pos++;
-        }
-
-        NumberSystem numsys = attributes.get(Attributes.NUMBER_SYSTEM, NumberSystem.ARABIC);
-        int total = 0;
-        boolean decimal = numsys.isDecimal();
-        int minPos = pos + 1;
-        int maxPos = (decimal ? pos + 2 : pos + 9); // safe
-
-        if (decimal) {
-            char zeroDigit = numsys.getDigits().charAt(0);
-            while (pos < maxPos) {
-                int digit = text.charAt(pos) - zeroDigit;
-
-                if ((digit >= 0) && (digit <= 9)) {
-                    total = total * 10 + digit;
-                    pos++;
-                } else {
-                    break;
-                }
-            }
-        } else {
-            while (pos < maxPos) {
-                if (numsys.contains(text.charAt(pos))) {
-                    pos++;
-                } else {
-                    break;
-                }
-            }
-
-            try {
-                if (pos >= minPos) {
-                    Leniency leniency = attributes.get(Attributes.LENIENCY, Leniency.SMART);
-                    total = numsys.toInteger(text.subSequence(minPos - 1, pos).toString(), leniency);
-                }
-            } catch (NumberFormatException nfe) {
-                status.setErrorIndex(start);
-                return null;
-            }
-        }
-
-        if ((pos < minPos) || (total < 1) || (total > 12)) {
-            status.setErrorIndex(start);
-            return null;
-        }
-
-        EastAsianMonth month = EastAsianMonth.valueOf(total);
-
-        if (leap) {
-            month = month.withLeap();
-        }
-
-        status.setIndex(pos);
-        return month;
+        throw new AbstractMethodError("Not yet available.");
 
     }
 
